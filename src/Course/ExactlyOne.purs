cad89dfb2631cbd8568bb502d578d9a3c31c66e3
Dolensek <Dolensek.Ryan@mayo.edu>
2020-10-19 16:33:30 -0500

    cleaned up ExactlyOne file

--- src/Course/ExactlyOne.purs
+++ src/Course/ExactlyOne.purs
@@ -1,39 +1,38 @@
 module Course.ExactlyOne where
 
 import Prelude
--- import Control.Applicative as A
-import Control.Monad as M
 
-data ExactlyOne a = ExactlyOne a --deriving (Eq, Show)
+data ExactlyOne a = ExactlyOne a
 
---derive instance exactlyOneEq :: Eq (ExactlyOne a)
---derive instance exactlyOneShow :: Show a => Show (ExactlyOne a)
+derive instance eqExactlyOne :: Eq a => Eq (ExactlyOne a)
 
 instance showExactlyOne :: Show a => Show (ExactlyOne a) where
+    show :: forall a. Show a => ExactlyOne a -> String
     show (ExactlyOne a) = show a
 
 runExactlyOne :: forall a. ExactlyOne a -> a
 runExactlyOne (ExactlyOne a) = a
 
 mapExactlyOne :: forall a b. (a -> b) -> ExactlyOne a -> ExactlyOne b
-mapExactlyOne f (ExactlyOne a)    = ExactlyOne (f a)
+mapExactlyOne f (ExactlyOne a) = ExactlyOne (f a)
 
 bindExactlyOne :: forall a b. (a -> ExactlyOne b) -> ExactlyOne a -> ExactlyOne b
 bindExactlyOne f (ExactlyOne a) = f a
 
 instance functorExactlyOne :: Functor ExactlyOne where
-  map = M.liftM1
+  map :: forall a b. (a -> b) -> ExactlyOne a -> ExactlyOne b
+  map = liftM1
 
 instance applicativeExactlyOne :: Applicative ExactlyOne where
+  pure :: forall a. a -> ExactlyOne a
   pure = ExactlyOne
 
 instance applyReader :: Apply ExactlyOne where
-  -- apply :: forall a b. f (a -> b) -> f a -> f b
+  apply :: forall a b. ExactlyOne (a -> b) -> ExactlyOne a -> ExactlyOne b
   apply (ExactlyOne f) (ExactlyOne r) = ExactlyOne (f r)
 
 instance bindExactlyOne1 :: Bind ExactlyOne where
---   (>>=) =
---     flip bindExactlyOne
+  bind :: forall a b. ExactlyOne a -> (a -> ExactlyOne b) -> ExactlyOne b
   bind = flip bindExactlyOne
 
 instance monadExactlyOne :: Monad ExactlyOne  
