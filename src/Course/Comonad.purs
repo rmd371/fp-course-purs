0df32d8a72efb8a03250c87be6e70f40fdf31177
rmd371 <rmd371@yahoo.com>
2021-01-29 19:21:14 -0600

    Comonad solution

--- src/Course/Comonad.purs
+++ src/Course/Comonad.purs
@@ -4,7 +4,6 @@ import Course.Core
 import Course.ExactlyOne
 import Course.Extend
 
-import Unsafe.Coerce (unsafeCoerce)
 import Course.Functor (map)
 
 -- | All instances of the `Comonad` type-class must satisfy two laws. These
@@ -16,31 +15,21 @@ import Course.Functor (map)
 -- * The law of right identity
 --   `∀f. copure . (f <<=) == f
 class Extend f <= Comonad f where
-  copure :: forall a.
-    f a
-    -> a
+  copure :: forall a. f a -> a
 
 -- | Implement the @Comonad@ instance for @ExactlyOne@.
 --
 -- >>> copure (ExactlyOne 7)
 -- 7
 instance exactlyOneComonad :: Comonad ExactlyOne where
-  copure :: forall a.
-    ExactlyOne a
-    -> a
-  copure = \ea -> unsafeCoerce "todo: Course.Comonad copure#instance ExactlyOne"
-    --error "todo: Course.Comonad copure#instance ExactlyOne"
+  copure :: forall a. ExactlyOne a -> a
+  copure (ExactlyOne a) = a
 
 -- | Witness that all things with (<<=) and copure also have (<$>).
 --
 -- >>> (+10) <$$> ExactlyOne 7
 -- ExactlyOne 17
-mapComonad :: forall f a b.
-  Comonad f =>
-  (a -> b)
-  -> f a
-  -> f b
-mapComonad = \f fa -> map (\_ -> unsafeCoerce "todo: Course.Comonad#(<$>)") fa
---   error "todo: Course.Comonad#(<$>)"
+mapComonad :: forall f a b. Comonad f => (a -> b) -> f a -> f b
+mapComonad = map
 
 infixl 4 mapComonad as <$$>
